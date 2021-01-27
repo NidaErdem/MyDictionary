599b5378caeb40e627a6b90c0aae06098f1c71ab
Nida Erdem <nidaerdem1@gmail.com>
2021-01-27 17:22:17 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1 , T2>
+    {
+        T1[] keys;
+        T2[] values;
+        T1[] tempKeys;      //geçici arrayler(temps).. yeni eleman girişi yapıldığında önceki verileri bu arraylerde tutarız.. Emaneten ;)
+        T2[] tempValues;
+
+        public MyDictionary()           //MyDictionary'i newlediğim an bu çalışır..
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            tempKeys = keys;        //tempsKeys keys'in referansını tutuyor.. Önce bu geçici listeleri tanımlayalım sonra eleman sayısını (Lenght) arttıracağız..
+            tempValues = values;    //""
+
+
+
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+
+
+            for (int j = 0; j < tempValues.Length; j++)
+            {
+                values[j] = tempValues[j];
+            }
+
+            values[values.Length - 1] = value;
+           
+
+        }
+
+
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+
+            Ogrenci.Add(2035, "Nida Erdem");
+            Ogrenci.Add(628, "Kaan Bakır");
+            Ogrenci.Add(1062, "İpek Dolu");
+            Ogrenci.Add(235, "Kübra Yurdagül");
+            Ogrenci.Add(1682, "Hakan Yılmaz");
+
+            Console.WriteLine(Ogrenci.Count);
+        }
+    }
+}
